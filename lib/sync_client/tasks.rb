68575e9674eaf164fc3da4b4c9e9c3eab060f308
Bryce Schmidt <bryce.schmidt@tstmedia.com>
2013-05-20 12:00:03 -0500

    loading envirnment and added logging

--- lib/sync_client/tasks.rb
+++ lib/sync_client/tasks.rb
@@ -3,11 +3,19 @@ namespace :sync_client do
   task :setup
 
   desc "Start a queue poller"
-  task :subscribe do
+  task :poll => :setup do
     ::Queuel.receive do |message|
       begin
-        SyncClient::SubMessage.new(JSON.parse(message.body)).process
-      rescue
+        puts "recieved message: #{message}"
+        process = nil
+        if message and message.body
+          process = SyncClient::SubMessage.new(JSON.parse(message.body)).process
+        end
+        puts "finished with: #{process}"
+        return process
+      rescue Exception => e
+        puts "exception occrued: #{message}"
+        puts "exception occrued: #{e}"
         # TODO log failure
         # return false to requeue the message, perhaps in failure queue
         false
