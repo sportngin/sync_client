b202370cf5ba1f04993881da98c519d7f5802cbe
Bryce Schmidt <bryce.schmidt@tstmedia.com>
2013-07-11 12:00:04 -0500

    refactored sync preference and actions in their own seperate classes

--- lib/sync_client/queue_publisher.rb
+++ lib/sync_client/queue_publisher.rb
@@ -12,9 +12,7 @@ module SyncClient
 
     def publish(action, object)
       sync_queue.each do |sync_queue|
-        if update?(action, object) and resolve_condition(object, sync_queue.options)
-          queue_message(:update, object, sync_queue.queue) if sync_queue.callbacks.include?(action)
-        end
+        queue_message(action, object, sync_queue.queue).publish if sync_queue.publishable?(action, object)
       end
     end
 
@@ -24,17 +22,6 @@ module SyncClient
         :action => action,
         :object_type => object.class.to_s,
         :object_attributes => object.publisher_attributes)
-    end
-
-    def update?(action, object)
-      action != :update || sync_queue.attributes.any?{|attr| object.send("#{attr}_changed?")}
-    end
-
-    def resolve_condition(object, options)
-      return true if options[:if].nil? && options[:unless].nil?
-      result = options[:if] == true || (options[:if].respond_to?(:call) && options[:if].call(object)) if options.has_key?(:if)
-      result = options[:unless] == false || (options[:unless].respond_to?(:call) && !options[:unless].call(object)) if options.has_key?(:unless)
-      result
     end
   end
 end
--- lib/sync_client/sync_queue.rb
+++ lib/sync_client/sync_queue.rb
@@ -5,11 +5,28 @@ module SyncClient
     attr_accessor :callbacks
     attr_accessor :options
 
+    CALLBACK_DEFAULTS = [:update, :create, :destroy]
+
     def initialize(attributes, options)
       @attributes = attributes
       @queue = options[:to]
-      @callbacks = options[:for] || [:update, :save, :destroy]
+      @callbacks = options[:for] || CALLBACK_DEFAULTS
       @options = options
+    end
+
+    def publishable?(action, object)
+      callbacks.include?(action) and update?(action, object) and resolve_condition(object)
+    end
+
+    def update?(action, object)
+      action.to_s != 'update' || attributes.any?{|attr| object.send("#{attr}_changed?")}
+    end
+
+    def resolve_condition(object)
+      return true if options[:if].nil? && options[:unless].nil?
+      result = options[:if] == true || (options[:if].respond_to?(:call) && options[:if].call(object)) if options.has_key?(:if)
+      result = options[:unless] == false || (options[:unless].respond_to?(:call) && !options[:unless].call(object)) if options.has_key?(:unless)
+      result
     end
   end
 end
