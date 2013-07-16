f03901c0d07f3bd74f0724376577d197b523e77b
Bryce Schmidt <bryce.schmidt@tstmedia.com>
2013-07-16 11:13:53 -0500

    further logging

--- lib/sync_client/queue_publisher.rb
+++ lib/sync_client/queue_publisher.rb
@@ -12,7 +12,7 @@ module SyncClient
 
     def publish(action, object)
       sync_queues.each do |sync_queue|
-        SyncClient.logger.warn("sync_queue queue blank: #{sync_queue.inspect}") if sync_queue.queue.blank?
+        SyncClient.logger.info("#{sync_queue.inspect}")
         queue_message(action, object, sync_queue.queue).publish if sync_queue.publishable?(action, object)
       end
     end
