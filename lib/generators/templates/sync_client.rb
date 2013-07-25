c06ff8d4c9e5166d7f7d57b0d8ee8e8601e3f93d
Bryce Schmidt <bryce.schmidt@tstmedia.com>
2013-07-25 10:37:51 -0500

    updated template to account for new options

--- lib/generators/templates/sync_client.rb
+++ lib/generators/templates/sync_client.rb
@@ -5,8 +5,9 @@ SyncClient.config do |config|
     # c.credentials token: 'asdufasdf8a7sd8fa7sdf', project_id: 'project_id'
     # c.engine :iron_mq
   end
-  # config.priotity_queue 'Resque'
+  # config.task_queue Resque
   # config.queue_suffix 'suffix'
+  # config.logger Logger.new
   # config.add_message_object_handler object_name, handler_class, actions
   # config.add_message_object_handler 'StatNgin::Game', 'Resource::StatNgin::Game', [:update, :create, :destroy]
 end
