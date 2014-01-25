81c1198c3c8035e75d559dc3e036de5434a4f794
Nick LaMuro <nicklamuro@gmail.com>
2014-01-24 18:33:37 -0600

    Move common code to a base module

--- lib/sync_client/publisher/mongoid.rb
+++ lib/sync_client/publisher/mongoid.rb
@@ -1,37 +1,12 @@
+require 'sync_client/publisher/base'
+
 module SyncClient
   module Publisher
     module Mongoid
       extend ActiveSupport::Concern
 
       included do
-        after_create :publish_create
-        before_update :publish_update
-        after_destroy :publish_destroy
-      end
-
-      module ClassMethods
-        def publish_changes_of(*attributes, options)
-          @queue_publisher = SyncClient::QueuePublisher.new unless @queue_publisher
-          @queue_publisher.add_publisher(attributes, options)
-        end
-
-        attr_reader :queue_publisher
-      end
-
-      def queue_publisher
-        self.class.queue_publisher
-      end
-
-      def publish_create
-        queue_publisher.publish(:create, self)
-      end
-
-      def publish_update
-        queue_publisher.publish(:update, self)
-      end
-
-      def publish_destroy
-        queue_publisher.publish(:destroy, self)
+        include SyncClient::Publisher::Base
       end
 
       def any_attributes_changed?(attributes)
