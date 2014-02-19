a773fa07932c11751579a2063a1c063b335b11e1
Carl Allen <carl.allen@sportngin.com>
2014-02-19 16:33:24 -0600

    Message error logging

--- lib/sync_client/worker.rb
+++ lib/sync_client/worker.rb
@@ -4,8 +4,12 @@ module SyncClient
       puts "starting queuel worker"
       ::Queuel.receive do |message|
         begin
-          SyncClient::SubMessage.new(message.body).process if message and message.body
-        rescue Exception => e
+          if message and message.body
+            SyncClient::SubMessage.new(message.body).process || SyncClient.logger.error("Message failed to process #{message.body}")
+          else
+            SyncClient.logger.error("Message invalid: #{message}")
+          end
+        rescue StandardError => e
           SyncClient.logger.error("Exception Occurred:\n\t#{e.message}\n\t#{e.backtrace}")
           false
         end
