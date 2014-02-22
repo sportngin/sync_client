8cd0ed1a944f70e1a346f6c5d97f6d0703cb5d77
Bryce Schmidt <bryce.schmidt@tstmedia.com>
2014-02-22 17:46:40 -0600

    added basic controller and engine code

--- lib/sync_client/engine.rb
+++ lib/sync_client/engine.rb
@@ -1,6 +1,5 @@
 module SyncClient
   class Engine < ::Rails::Engine
-
     initializer "sync_client.eager_load", after: :set_autoload_paths do |app|
       paths["app/models"].each do |load_path|
         matcher = /\A#{Regexp.escape(load_path)}\/(.*)\.rb\Z/
